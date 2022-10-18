3171a0ed7fd54f197d1a90bf44147e2ac2fb117b
Ryli Davis <ryli_davis@intuit.com>
2022-10-18 12:49:35 -0700

    testing: pay button becomes enabled

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo"
 
 test('on initial render, the pay button is disabled', async () => {
@@ -10,6 +11,21 @@ error where the pay button is enabled for half a second then is disabled. Becaus
 we need to use async await. Since we use async await, instead of getByRole, we use
 findByRole */
 expect(await screen.findByRole('button', {name: /pay/i})).toBeDisabled();
+} );
+
+
+
+/*In this test, we also want to render the transaction create step 2 with these props. 
+We want to mimic the event where the user is entering an amount and adds a note*/
+test('if an amount and note is entered, the pay button becomes enabled', async () => {
+
+    render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}} />)
+
+userEvent.type(screen.getByPlaceholderText(/amount/i), "50")
+userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner")
+
+expect(await screen.findByRole('button', {name: /pay/i})).toBeEnabled();
+
 } );
 
 
